7d53134025212ea9b35a487485a4de6b4c98518b
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-28 10:14:43 +0700

    Setting up the project

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || 'mhomhomhomhomho',
+  mongoUri:
+    process.env.MONGODB_URI
+    || process.env.MONGO_HOST
+    || `mongodb://${process.env.IP || 'localhost'}:${
+      process.env.MONGO_PORT || '27017'
+    }/unicornshells-vof`,
+};
+
+export default config;
