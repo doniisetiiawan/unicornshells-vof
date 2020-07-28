05b9071389450b3aa88db93dbd0e96afaf07880e
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-28 14:11:19 +0700

    Backend API integration

---
+++ client/auth/api-auth.js
@@ -0,0 +1,19 @@
+const signin = (user) => fetch('/auth/signin/', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  credentials: 'include',
+  body: JSON.stringify(user),
+})
+  .then((response) => response.json())
+  .catch((err) => console.log(err));
+
+const signout = () => fetch('/auth/signout/', {
+  method: 'GET',
+})
+  .then((response) => response.json())
+  .catch((err) => console.log(err));
+
+export { signin, signout };
